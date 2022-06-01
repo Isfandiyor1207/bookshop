ca7b189d8b0fe1ee4b1d1bf7a15af9d742ad99bc
isfandiyor <sultonovisfandiyor0@gmail.com>
2022-06-01 20:52:55 +0500

    Service Push

---
+++ src/main/java/epam/project/bookshop/service/BookService.java
@@ -0,0 +1,7 @@
+package epam.project.bookshop.service;
+
+import epam.project.bookshop.entity.Book;
+import epam.project.bookshop.entity.User;
+
+public interface BookService extends GenericService<Book>{
+}
---
+++ src/main/java/epam/project/bookshop/service/GenreService.java
@@ -0,0 +1,7 @@
+package epam.project.bookshop.service;
+
+import epam.project.bookshop.entity.Genre;
+import epam.project.bookshop.entity.User;
+
+public interface GenreService extends GenericService<Genre> {
+}
